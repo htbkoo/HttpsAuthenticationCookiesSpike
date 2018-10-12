81742693fe4c4aecc878bcc9246cc0971249a8af
htbkoo <h1017760@connect.hku.hk>
2018-10-12 19:45:53 +0800

    removed unused custom Next.js configuration htbkoo/AnalyticsAndAdsSpike

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
 const withTypescript = require('@zeit/next-typescript');
-module.exports = withTypescript({
-    webpack(config, options) {
-        return config;
-    }
-});
+module.exports = withTypescript();
