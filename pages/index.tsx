1186c09255f1c67019bdc166fa9b40098cd61d05
htbkoo <h1017760@connect.hku.hk>
2018-10-13 23:10:21 +0800

    added PostsList with usage of styledJsx htbkoo/AnalyticsAndAdsSpike

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,48 @@ import fetch from 'isomorphic-unfetch';
 
 import Layout from "../components/MyLayout";
 
+const PostsList = ({posts}) => (
+    <div>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>
+                    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+                        <a>{post.title}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+    </div>
+);
+
 const ShowsList = ({shows}) => (
     <ul>
         {shows.map(({show}) => (
             <li key={show.id}>
-                <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                <Link as={`/s/${show.id}`} href={`/show?id=${show.id}`}>
                     <a>{show.name}</a>
                 </Link>
             </li>
@@ -19,6 +56,7 @@ const ShowsList = ({shows}) => (
 const Index = (props) => (
     <Layout>
         <h1>My Blog</h1>
+        <PostsList posts={getPosts()}/>
         <ShowsList shows={props.shows}/>
     </Layout>
 );
@@ -33,5 +71,13 @@ Index.getInitialProps = async function () {
         shows: data
     };
 };
+
+function getPosts() {
+    return [
+        {id: 'hello-nextjs', title: 'Hello Next.js'},
+        {id: 'learn-nextjs', title: 'Learn Next.js'},
+        {id: 'deploy-nextjs', title: 'Deploy Next.js'},
+    ];
+}
 
 export default Index;
