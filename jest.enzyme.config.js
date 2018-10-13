4acee6b6124e0d3c945ef1292024f9f94b54a041
htbkoo <h1017760@connect.hku.hk>
2018-10-13 17:20:24 +0800

    updated jest.enzyme.config.js htbkoo/AnalyticsAndAdsSpike

--- jest.enzyme.config.js
+++ jest.enzyme.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    "displayName": "Enzyme Tests",
+    "testMatch": [
+        '**/*.(test|spec).(tsx|jsx)'
+    ],
     "preset": 'ts-jest',
     "testEnvironment": 'jsdom',
     "testPathIgnorePatterns": ["/node_modules/", "/build/.*", "/\.next/.*"],
