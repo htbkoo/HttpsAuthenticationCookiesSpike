8623df4fd7340654c9a26f0f188cc8524f528127
htbkoo <h1017760@connect.hku.hk>
2018-10-17 17:31:33 +0800

    reused <Layout/> htbkoo/AnalyticsAndAdsSpike

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,8 +3,9 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Link from 'next/link';
+import Layout from "../src/main/typescript/components/MyLayout";
 
 const styles = theme => ({
     root: {
@@ -13,26 +14,28 @@ const styles = theme => ({
     },
 });
 
-function About(props: {classes: {root: string}}) {
-    const { classes } = props;
+function About(props: { classes: { root: string } }) {
+    const {classes} = props;
 
     return (
-        <div className={classes.root}>
-            <Typography variant="h4" gutterBottom>
-                Material-UI
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-                about page
-            </Typography>
-            <Typography gutterBottom>
-                <Link href="/">
-                    <a>Go to the main page</a>
-                </Link>
-            </Typography>
-            <Button variant="contained" color="primary">
-                Do nothing button
-            </Button>
-        </div>
+        <Layout>
+            <div className={classes.root}>
+                <Typography variant="h4" gutterBottom>
+                    Material-UI
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                    about page
+                </Typography>
+                <Typography gutterBottom>
+                    <Link href="/">
+                        <a>Go to the main page</a>
+                    </Link>
+                </Typography>
+                <Button variant="contained" color="primary">
+                    Do nothing button
+                </Button>
+            </div>
+        </Layout>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 import Link from 'next/link';
 
+import Layout from "../src/main/typescript/components/MyLayout";
+
 const styles = theme => ({
     root: {
         textAlign: 'center',
@@ -19,7 +21,7 @@ const styles = theme => ({
 });
 
 type IndexProps = {
-    classes: {root: string}
+    classes: { root: string }
 }
 
 type IndexState = {
@@ -48,33 +50,35 @@ class Index extends React.Component<IndexProps, IndexState> {
         const {open} = this.state;
 
         return (
-            <div className={classes.root}>
-                <Dialog open={open} onClose={this.handleClose}>
-                    <DialogTitle>Super Secret Password</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>1-2-3-4-5</DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button color="primary" onClick={this.handleClose}>
-                            OK
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-                <Typography variant="h4" gutterBottom>
-                    Material-UI
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                    example project
-                </Typography>
-                <Typography gutterBottom>
-                    <Link href="/about">
-                        <a>Go to the about page</a>
-                    </Link>
-                </Typography>
-                <Button variant="contained" color="secondary" onClick={this.handleClick}>
-                    Super Secret Password
-                </Button>
-            </div>
+            <Layout>
+                <div className={classes.root}>
+                    <Dialog open={open} onClose={this.handleClose}>
+                        <DialogTitle>Super Secret Password</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>1-2-3-4-5</DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button color="primary" onClick={this.handleClose}>
+                                OK
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                    <Typography variant="h4" gutterBottom>
+                        Material-UI
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        example project
+                    </Typography>
+                    <Typography gutterBottom>
+                        <Link href="/about">
+                            <a>Go to the about page</a>
+                        </Link>
+                    </Typography>
+                    <Button variant="contained" color="secondary" onClick={this.handleClick}>
+                        Super Secret Password
+                    </Button>
+                </div>
+            </Layout>
         );
     }
 }
