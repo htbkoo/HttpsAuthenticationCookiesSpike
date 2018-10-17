581689653253713ec7fce3f0481e8afa8946fd00
htbkoo <h1017760@connect.hku.hk>
2018-10-17 17:15:34 +0800

    updated about.tsx htbkoo/AnalyticsAndAdsSpike

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
-import Layout from "../components/MyLayout";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
 
-export default () => (
-    <Layout>
-        <p>This is the about page</p>
-    </Layout>
-);
+const styles = theme => ({
+    root: {
+        textAlign: 'center',
+        paddingTop: theme.spacing.unit * 20,
+    },
+});
+
+function About(props) {
+    const { classes } = props;
+
+    return (
+        <div className={classes.root}>
+            <Typography variant="h4" gutterBottom>
+                Material-UI
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+                about page
+            </Typography>
+            <Typography gutterBottom>
+                <Link href="/">
+                    <a>Go to the main page</a>
+                </Link>
+            </Typography>
+            <Button variant="contained" color="primary">
+                Do nothing button
+            </Button>
+        </div>
+    );
+}
+
+About.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(About);
