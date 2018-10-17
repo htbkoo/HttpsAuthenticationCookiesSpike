da956c44449b95f74db90269d0673fc16bbbbdff
htbkoo <h1017760@connect.hku.hk>
2018-10-17 18:58:27 +0800

    migrated <Header/> to be MUI.AppBar htbkoo/HttpsAuthenticationCookiesSpike

--- src/main/typescript/components/Header.tsx
+++ src/main/typescript/components/Header.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import {createStyles, Typography, withStyles, WithStyles} from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 import NavBar from "./NavBar";
 import UserInfo from "./UserInfo";
-import {createStyles, withStyles, WithStyles} from "@material-ui/core";
 
-const styles = () => createStyles({
-    header: {
-        display: "flex"
+const styles = createStyles({
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
     },
 });
 
-interface Props extends WithStyles<typeof styles>{
-
+interface UserInfoProps extends WithStyles<typeof styles> {
 }
 
-const Header = (props:Props) => (
-    <div className={props.classes.header}>
-        <NavBar/>
-        <UserInfo/>
-    </div>
-);
+function Header(props: UserInfoProps) {
+    const {classes} = props;
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <NavBar/>
+                    <Typography variant="h6" color="inherit" className={classes.grow}/>
+                    <UserInfo/>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
 
 export default withStyles(styles)(Header);
